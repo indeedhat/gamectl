d2e598a6170b9bad46521a4c81728b0552326262
kagorus <nathan@kagorus.net>
2021-08-15 20:14:55 +0100

    Matt said push.

---
+++ assets/js/updates.js
@@ -0,0 +1,6 @@
+// Updates css colors if a server status changes, should be called from main.js each time servers 
+// are pinged for status
+function update_css(){
+    var status = document.getElementsByClassName("status");
+
+}
