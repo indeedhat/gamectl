c1e0598872ebc1a8c2389af437aa1e61e980d2de
kagorus <nathan@kagorus.net>
2021-08-15 21:12:19 +0100

    Nightly Finishes

--- assets/js/updates.js
+++ assets/js/updates.js
@@ -1,9 +1,14 @@
 // Updates css colors if a server status changes, should be called from main.js each time servers 
 // are pinged for status
 
-//Remove Me Later Dumbass.
-update_css();
+
 function update_css(){
     var status = document.getElementsByClassName("status");
-    console.log(status);
+    
+
+    let text = "";
+    for (let x in status) {
+    text = status.innerHTML;
+    console.log(text);
+    }
 }
