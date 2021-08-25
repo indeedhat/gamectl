51f27ff753f2be6d51e3228b96b074a9577d00e8
indeedhat <iam4423@gmail.com>
2021-08-25 21:55:58 +0100

    added download world button

--- assets/js/world-download.js
+++ assets/js/world-download.js
@@ -5,4 +5,32 @@ once(
     () => loadJs("/assets/js/lib/js-file-downloader.min.js")
 );
 
+const initDownloadButton = (key, $elem) => {
+    $elem.onclick = _buildDownloadEventHandler(key, $elem);
+};
 
+const _buildDownloadEventHandler = (key, $elem) => {
+    return async function() {
+        if (!confirm("The game server will be shut down while the backup us processed!\nDo you wish to continue?")) {
+            return;
+        }
+
+        let download = new jsFileDownloader({
+            url: `/api/apps/${key}/download`,
+            autoStart: false
+        });
+
+        $elem.innerHTML = "Please Wait...";
+
+        try {
+            await download.start();
+            $elem.innerHTML = "Download World";
+        } catch (e) {
+            $elem.innerHTML = "Download Failed!";
+        }
+    };
+};
+
+export {
+    initDownloadButton
+};
