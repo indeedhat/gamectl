3bf27d13dca88fa44e528d210937d6a953fb4c30
indeedhat <iam4423@gmail.com>
2021-11-02 21:48:05 +0000

    refactor: resource monitor into alpine driven component

--- web/assets/js/resource-monitor.js
+++ web/assets/js/resource-monitor.js
@@ -1,171 +1,131 @@
 import { fileSize, trafficSpeed, duration, once, loadCss, percentage } from '/assets/js/util.js';
 
-class ResourceMonitor
+/**
+ * Setup the Resource monitor components
+ *
+ * @async
+ */
+const createResourceMonitor = async () => {
+    importCss();
+
+    window.addEventListener("alpine:init", function() {
+        Alpine.data("resourceMonitor", () => ({
+            loading: true,
+            uptime: "string",
+            memory: {},
+            cpu: {},
+            mounts: {},
+            network: {},
+
+            init() {
+                new StreamHandler(this);
+            }
+        }));
+
+    });
+};
+
+const importCss = () => {
+    once(
+        "resource-monitor", 
+        () => loadCss('/assets/css/resource-monitor.css')
+    );
+};
+
+class StreamHandler
 {
-    constructor(selector)
+    constructor(monitorData)
     {
-        this.$monitor = document.querySelector(selector);
-        this.$data = this.$monitor.querySelector('.data');
+        this.stream = null;
+        this.monitor = monitorData;
 
-        this._stream = null;
+        this.setupEventSource();
 
-        this.$data.innerHTML = loadingTemplate();
-
-        this._importCss();
-        this._setupEventSource();
-        this._destructor();
-    }
-
-    _destructor()
-    {
         window.onbeforeunload = () => {
-            this._stream.close();
+            this.stream.close();
         };
     }
 
-    _importCss()
+    setupEventSource()
     {
-        once(
-            "resource-monitor", 
-            () => loadCss('/assets/css/resource-monitor.css')
-        );
+        this.stream = new EventSource("/api/performance");
+
+        this.stream.onerror = e => console.error("Resource Monitor:", e);
+        this.stream.addEventListener("message", this.handleMessage.bind(this));
     }
 
-    _setupEventSource()
+    handleMessage({ data })
     {
-        this._stream = new EventSource("/api/performance");
+        let { uptime, memory, cpu, network, mount } = JSON.parse(data);
 
-        this._stream.onerror = this._handleError.bind(this);
-        this._stream.addEventListener("message", this._handleMessage.bind(this));
-    }
-
-    _handleError(e)
-    {
-        console.error("Resource Monitor:", e);
-    }
-
-    _handleMessage({ data })
-    {
-        this.$data.innerHTML = buildTemplate(JSON.parse(data));
+        this.monitor.loading = false;
+        this.monitor.uptime = duration(uptime);
+        this.monitor.cpu = buildCpu(cpu);
+        this.monitor.memory = buildMemory(memory);
+        this.monitor.mounts = buildMounts(mount);
+        this.monitor.network = buildNetwork(network);
     }
 }
 
-const loadingTemplate = () => {
-    return `
-        <table>
-            <tr>
-                <td>Loading System Resources...</td>
-            </tr>
-        </table>
-    `;
-}
-
-const buildTemplate = ({ uptime, memory, cpu, network, mount }) => {
-    let coreTemplate = "";
-    let networkTemplate = "";
-    let mountTemplate = "";
+const buildCpu = cpu => {
     let cpuTotal = 0;
     let cpuIdle = 0;
+    let cores = [];
 
     for (let [ key, core ] of Object.entries(sortCores(cpu))) {
         cpuTotal += core.total;
         cpuIdle += core.idle;
 
-        let ptg = percentage(core.total, core.total - core.idle);
-        coreTemplate += `<div class="core">
-                <div class="key">${key}:</div>
-                ${progressBar(ptg, ptg, `rgb(${cpuTemp(ptg)})`)}
-            </div>`;
+        let corePercent = percentage(core.total, core.total - core.idle);
+
+        cores.push({
+            key,
+            text: corePercent,
+            width: corePercent,
+            color: `rgb(${cpuTemp(corePercent)})`
+        });
     }
 
-    for (let [ key, intf ] of Object.entries(network)) {
-        networkTemplate += `
-            <div class="intf">
-                <strong>${key}:</strong>
-                <div>Tx: ${trafficSpeed(intf.tx)}</div>
-                <div>Rx: ${trafficSpeed(intf.rx)}</div>
-            </div>
-        `;
-    }
-
-    for (let [ key, mnt ] of Object.entries(mount)) {
-        mountTemplate += `
-            <div class="mnt">
-                <div class="key">${key}:</div>
-                ${progressBar(
-                    `${fileSize(mnt.used)} / ${fileSize(mnt.total)}`,
-                    percentage(mnt.total, mnt.used),
-                    `rgb(${cpuTemp(percentage(mnt.total, mnt.used))})`
-                )}
-            </div>
-        `;
-    }
-
-    return `
-        <div class="sysStatus">
-            <div class="sysItem">
-                <div class="sysLabel"> 
-                    Uptime
-                </div>
-            
-                <div class="sysData">
-                    ${duration(uptime)}
-                </div>
-            </div>
-
-            <div class="sysItem">
-                <div class="sysLabel"> 
-                    Memory
-                </div>
-        
-                <div class="sysData">
-                    ${progressBar(
-                        `${fileSize(memory.used)} / ${fileSize(memory.total)}`,
-                        percentage(memory.total, memory.used),
-                        `rgb(${cpuTemp(percentage(memory.total, memory.used))})`
-                    )}
-                </div>
-            </div>
-
-            <div class="sysItem">
-                <div class="sysLabel"> 
-                    CPU
-                </div>
-        
-                <div class="sysData">
-                    ${progressBar(
-                        `<strong>Total:</strong>${percentage(cpuTotal, cpuTotal - cpuIdle)}`, 
-                        percentage(cpuTotal, cpuTotal - cpuIdle),
-                        `rgb(${cpuTemp(percentage(cpuTotal, cpuTotal - cpuIdle))})`
-                    )}
-                    ${coreTemplate}
-                </div>
-            </div>
-
-            <div class="sysItem">
-                <div class="sysLabel"> 
-                    Mounts
-                </div>
-        
-                <div class="sysData">
-                    ${mountTemplate}
-                </div>
-            </div>
-
-            <div class="sysItem">
-                <div class="sysLabel"> 
-                    Network
-                </div>
-        
-                <div class="sysData">
-                    ${networkTemplate}
-                </div>
-            </div>            
-             
-            <div class="clearfix"></div>          
-        </div>
-    `;
+    let cpuPercent = percentage(cpuTotal, cpuTotal - cpuIdle);
+    return {
+        text: cpuPercent,
+        width: cpuPercent,
+        color: `rgb(${cpuTemp(cpuPercent)})`,
+        cores
+    };
 };
+
+const buildMemory = memory => {
+    let memoryPercent = percentage(memory.total, memory.used);
+
+    return {
+        text: fileSize(memory.used) + " / " + fileSize(memory.total),
+        width: memoryPercent,
+        color: `rgb(${cpuTemp(memoryPercent)})`
+    };
+}
+
+const buildMounts = mount => {
+    return mount.map((mnt, key) => {
+        let mountPercent = percentage(mnt.total, mnt.used);
+
+        return {
+            key,
+            text: `${fileSize(mnt.used)} / ${fileSize(mnt.total)}`,
+            width: mountPercent,
+            color: `rgb(${cpuTemp(mountPercent)})`
+        };
+    });
+};
+
+const buildNetwork = network => {
+    return network.map((intf, key) => ({
+        key,
+        tx: trafficSpeed(intf.tx),
+        rx: trafficSpeed(intf.rx)
+    }));
+};
+
 
 const sortCores = cpu => {
     return Object.keys(cpu)
@@ -182,15 +142,4 @@ const sortCores = cpu => {
         }, {})
 };
 
-const progressBar = (text, width, color) => {
-    return `
-        <div class="progress">
-            <span class="value">${text}</span>
-            <div class="bar" style="width:${width};background:${color};">
-                <span class="value">${text}</span>
-            </div>
-        </div>
-    `;
-};
-
-export default ResourceMonitor;
+export default createResourceMonitor;
